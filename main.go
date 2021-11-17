a16d62ef7c43599f454e2207a727d18fb56d2cf8
FutureCoder <FutureCoder@aliyun.com>
2021-11-17 08:52:00 +0800

    update

--- main.go
+++ main.go
@@ -11,8 +11,10 @@ import (
 func main() {
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
 	}
+	cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -20,4 +22,5 @@ func main() {
 	if err := cmd.Run(); err != nil {
 		log.Fatalln(err)
 	}
+	os.Exit(-1)
 }
